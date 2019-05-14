032ada0fdfad86032ff9a047fa58db3c69039005
Ilonavdh <44259176+Ilonavdh@users.noreply.github.com>
2019-05-14 11:57:30 +0200

    Add files via upload

---
+++ precache-manifest.88cc9f37c7f721697287cec0402f4bcb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c79800ad9ef80662d53f77f1d15f78a5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b2eb6fd8f2ddcb35869a",
+    "url": "/static/css/main.b97dd832.chunk.css"
+  },
+  {
+    "revision": "3f49cb5d619f8b5f02ce",
+    "url": "/static/js/2.bcbbb54f.chunk.js"
+  },
+  {
+    "revision": "b2eb6fd8f2ddcb35869a",
+    "url": "/static/js/main.f9497647.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
